d0206c702cc1db604ceb95ab4cf727a51e60b6fd
尹志祥 <13602547976@163.com>
2022-03-04 23:02:24 +0800

    md

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+    
+  },
+  data(){
+    return {
+      
+    }
+  },
+  mounted(){
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
