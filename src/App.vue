81055b5f0b494e72818cff536bfa083130855458
尹志祥 <13602547976@163.com>
2022-03-05 22:21:00 +0800

    fix:storage

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import storage from './storage/index'
 export default {
   name: 'app',
   components: {
@@ -16,7 +17,9 @@ export default {
     }
   },
   mounted(){
-    
+    // storage.setItem('a',1)
+    // storage.setItem('abc',{a:1},'user')
+    storage.clear('abc','user');
   }
 }
 </script>
