a5436efaef6f57d8ceccc72d3ac852950dcb9225
尹志祥 <13602547976@163.com>
2022-03-06 23:46:07 +0800

    fix:one

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,5 +1,48 @@
 <template>
-  <div>nav-header</div>
+  <div class="header">
+    <div class="nav-topbar">
+      <div class="container">
+        <div class="topbar-menu">
+          <a href="javascript:;">小米商城</a>
+          <a href="javascript:;">MUI</a>
+          <a href="javascript:;">云服务</a>
+          <a href="javascript:;">协议规则</a>
+        </div>
+        <div class="topbar-user">
+          <a href="javascript:;">登录</a>
+          <a href="javascript:;">注册</a>
+          <a href="javascript:;" class="my-cart"><span class="icon-cart"></span>购物车</a>
+        </div>
+      </div>
+    </div>
+    <div class="nav-header">
+      <div class="container">
+        <div class="header-logo">
+          <a href="/#/index"></a>
+        </div>
+        <div class="header-menu">
+          <div class="item-menu">
+            <span>小米手机</span>
+            <div class="children"></div>
+          </div>
+          <div class="item-menu">
+            <span>RedMi手机</span>
+            <div class="children"></div>
+          </div>
+          <div class="item-menu">
+            <span>电视</span>
+            <div class="children"></div>
+          </div>
+        </div>
+        <div class="header-search">
+          <div class="wrapper">
+            <input type="text" name="keyword">
+            <a href="javascript:;"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +51,105 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  @import './../assets/scss/base.scss';
+  @import './../assets/scss/mixin.scss';
+  .header {
+    .nav-topbar {
+      height: 39px;
+      line-height: 39px;
+      background: #333333;
+      color: #B0B0B0;
+      .container {
+        width: 1226px;
+        margin: 0 auto;
+        @include flex();
+        a{
+          display: inline-block;
+          color: #B0B0B0;
+          margin-right: 17px;
+        }
+        .my-cart{
+          width: 110px;
+          text-align: center;
+          background: #FF6600;
+          color: #ffffff;
+          .icon-cart{
+            @include bgImg(16px,12px,'/imgs/icon-cart-checked.png');
+            margin-right: 4px;
+          }
+        }
+      }
+    }
+    .nav-header {
+      .container {
+        height: 112px;
+        @include flex();
+        .header-logo{
+          display: inline-block;
+          width: 55px;
+          height: 55px;
+          background: #FF6600;
+          a {
+            display: inline-block;
+            width: 110px;
+            height: 100%;
+            &:before{
+              content: "";
+              @include bgImg(55px,55px,'/imgs/mi-logo.png',55px);
+              transition: margin .2s;
+            }
+            &:after{
+              content: "";
+              @include bgImg(55px,55px,'/imgs/mi-home.png',55px);
+            }
+            &:hover:before{
+              margin-left: -55px;
+              transition: margin .2s;
+            }
+          }
+        }
+        .header-menu{
+          display: inline-block;
+          width: 643px;
+          padding-left: 209px;
+          .item-menu{
+            display: inline-block;
+            color: #333333;
+            font-weight: bold;
+            font-size: 16px;
+            line-height: 112px;
+            margin-right: 20px;
+            span {
+              cursor: pointer;
+            }
+            &:hover {
+              
+            }
+          }
+        }
+        .header-search{
+          width: 319px;
+          .wrapper{
+            height: 50px;
+            border: 1px solid #e0e0e0;
+            display: flex;
+            align-items: center;
+            input{
+              border: none;
+              border-right: 1px solid #e0e0e0;
+              width: 264px;
+              height: 50px;
+              padding-left: 14px;
+              box-sizing: border-box;
+            }
+            a{
+              @include bgImg(18px,18px,'/imgs/icon-search.png');
+              margin-left: 17px;
+            }
+          }
+        }
+      }
+    }
+  }
 </style>
