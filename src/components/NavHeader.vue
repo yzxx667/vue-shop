2f02abce3988e2f4dbd510cca12e49a9564c9bee
尹志祥 <13602547976@163.com>
2022-03-07 13:50:59 +0800

    finish:nav

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -9,9 +9,10 @@
           <a href="javascript:;">协议规则</a>
         </div>
         <div class="topbar-user">
-          <a href="javascript:;">登录</a>
-          <a href="javascript:;">注册</a>
-          <a href="javascript:;" class="my-cart"><span class="icon-cart"></span>购物车</a>
+          <a href="javascript:;" v-if="username">{{username}}</a>
+          <a href="javascript:;" v-if="!username" @click="login">登录</a>
+          <a href="javascript:;">我的订单</a>
+          <a href="javascript:;" class="my-cart" @click="goToCart"><span class="icon-cart"></span>购物车</a>
         </div>
       </div>
     </div>
@@ -23,15 +24,86 @@
         <div class="header-menu">
           <div class="item-menu">
             <span>小米手机</span>
-            <div class="children"></div>
+            <div class="children">
+              <ul>
+                <li class="product"
+                 v-for="(item,index) in phoneList"
+                 :key="index"
+                 >
+                  <a :href="`/#/product/${item.id}`" target="_blank">
+                    <div class="pro-img">
+                      <img :src="item.mainImage" :alt="item.subtitle">
+                    </div>
+                    <div class="pro-name">{{item.name}}</div>
+                    <div class="pro-price">{{item.price | currency}}元</div>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
           <div class="item-menu">
             <span>RedMi手机</span>
-            <div class="children"></div>
           </div>
           <div class="item-menu">
             <span>电视</span>
-            <div class="children"></div>
+            <div class="children">
+              <ul>
+                <li class="product">
+                  <a href="" target="_blank">
+                    <div class="pro-img">
+                      <img src="/imgs/nav-img/nav-3-1.jpg" alt="">
+                    </div>
+                    <div class="pro-name">小米壁画电视 65英寸</div>
+                    <div class="pro-price">6999元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href="" target="_blank">
+                    <div class="pro-img">
+                      <img src="/imgs/nav-img/nav-3-2.jpg" alt="">
+                    </div>
+                    <div class="pro-name">小米全面屏电视E55A</div>
+                    <div class="pro-price">1999元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href="" target="_blank">
+                    <div class="pro-img">
+                      <img src="/imgs/nav-img/nav-3-3.png" alt="">
+                    </div>
+                    <div class="pro-name">小米电视4A 32英寸</div>
+                    <div class="pro-price">699元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href="" target="_blank">
+                    <div class="pro-img">
+                      <img src="/imgs/nav-img/nav-3-4.jpg" alt="">
+                    </div>
+                    <div class="pro-name">小米电视4A 55英寸</div>
+                    <div class="pro-price">1799元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href="" target="_blank">
+                    <div class="pro-img">
+                      <img src="/imgs/nav-img/nav-3-5.jpg" alt="">
+                    </div>
+                    <div class="pro-name">小米电视4A 65英寸</div>
+                    <div class="pro-price">2699元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href="" target="_blank">
+                    <div class="pro-img">
+                      <img src="/imgs/nav-img/nav-3-6.png" alt="">
+                    </div>
+                    <div class="pro-name">查看全部</div>
+                    <div class="pro-price">查看全部</div>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <div class="header-search">
@@ -47,13 +119,51 @@
 
 <script>
 export default {
-  name: 'nav-header'
+  name: 'nav-header',
+  data () {
+    return {
+      username:'jack',
+      phoneList:[]
+    }
+  },
+  filters:{
+    currency(val){
+      if(!val)return '0.00';
+      return '¥' + val.toFixed(2) + '元'
+    }
+  },
+  mounted () {
+    this.getProductList();
+  },
+  methods:{
+    login(){
+      this.$router.push('/login');
+    },
+    getProductList(){
+      this.axios.get('/products',{
+        params:{
+          categoryId:'100012',
+          pageSize:6
+        }
+      }).then((res)=>{
+        if(res.list.length>=6){
+          this.phoneList =  res.list.slice(0,6)
+        }
+      })
+    },
+    goToCart(){
+      this.$router.push('/cart');
+    }
+  }
+  
+  
 }
 </script>
 
 <style lang="scss">
   @import './../assets/scss/base.scss';
   @import './../assets/scss/mixin.scss';
+  @import './../assets/scss/config.scss';
   .header {
     .nav-topbar {
       height: 39px;
@@ -84,6 +194,7 @@ export default {
     .nav-header {
       .container {
         height: 112px;
+        position: relative;
         @include flex();
         .header-logo{
           display: inline-block;
@@ -124,7 +235,64 @@ export default {
               cursor: pointer;
             }
             &:hover {
-              
+              color: $colorA;
+              .children{
+                height: 220px;
+                opacity: 1;
+              }
+            }
+            .children {
+              position: absolute;
+              top: 112px;
+              left: 0;
+              height: 0;
+              opacity: 0;
+              width: 1226px;
+              overflow: hidden;
+              border-top: 1px solid #e5e5e5;
+              box-shadow: 0px 7px 6px 0px rgba(0,0,0,0.11);
+              z-index: 10;
+              transition: .3s;
+              .product{
+                float: left;
+                width: 16.6%;
+                height: 220px;
+                font-size: 12px;
+                line-height: 12px;
+                text-align: center;
+                position: relative;
+                &:before{
+                  content: "";
+                  position: absolute;
+                  top: 28px;
+                  right: 0;
+                  border-left: 1px solid $colorF;
+                  height: 100px;
+                  width: 1px;
+                }
+                &:last-child:before{
+                  display: none;
+                }
+                a{
+                  display: inline-block;
+                }
+                img{
+                  margin-top: 26px;
+                  width: auto;
+                  height: 111px;
+                }
+                .pro-img{
+                  height: 137px;
+                }
+                .pro-name{
+                  font-weight: bold;
+                  margin: 19px 0 8px 0;
+                  color: $colorB;
+                }
+                .pro-price{
+                  color: $colorA;
+                }
+              }
             }
           }
         }
