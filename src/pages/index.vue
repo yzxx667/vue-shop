343348f23e88953c531b0f24fc019f1ce8d97657
尹志祥 <13602547976@163.com>
2022-03-07 21:05:52 +0800

    fix:半

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,163 @@
 <template>
-  <div> 
+  <div class="index">
+    <div class="container">
+      <div class="swiper-box">
+        <div class="nav-menu">
+          <ul class="menu-wrap">
+            <li class="menu-item">
+              <a href="javascript:;">手机 电话卡</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电视 盒子</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">笔记本 平板</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">家电 插线板</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">出行 穿戴</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">智能 路由器</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电源 配件</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">生活 箱包</a>
+            </li>
+          </ul>
+        </div>
+        <swiper :options="swiperOptions">
+          <swiper-slide v-for="(item, index) of slideList" :key="index">
+            <a href="'/#/product/'+item.id">
+              <img :src="item.img" />
+            </a>
+          </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </div>
+      <div class="ads"></div>
+      <div class="banner"></div>
+      <div class="product-box"></div>
+    </div>
     <service-bar></service-bar>
   </div>
 </template>
 
 <script>
-import ServiceBar from '../components/ServiceBar.vue'
+import ServiceBar from "../components/ServiceBar.vue";
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
 export default {
-  name: 'nav-home',
+  name: "nav-home",
   components: {
-    ServiceBar
-  }
-}
+    Swiper,
+    SwiperSlide,
+    ServiceBar,
+  },
+  data() {
+    return {
+      swiperOptions: {
+        autoplay: true,
+        loop: true,
+        effect: "cube",
+        cubeEffect: {
+          slideShadows: true,
+          //滑动阴影
+          shadow: false,
+          shadowOffset: 100,
+          shadowScale: 0.6,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable :true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
+      slideList: [
+        {
+          id: "42",
+          img: "/imgs/slider/slide-1.jpg",
+        },
+        {
+          id: "45",
+          img: "/imgs/slider/slide-2.jpg",
+        },
+        {
+          id: "46",
+          img: "/imgs/slider/slide-3.jpg",
+        },
+        {
+          id: "42",
+          img: "/imgs/slider/slide-4.jpg",
+        },
+        {
+          id: "42",
+          img: "/imgs/slider/slide-5.jpg",
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+@import './../assets/scss/mixin.scss';
+@import './../assets/scss/config.scss';
+.index {
+  .swiper-box {
+    width: 1226px;
+    .nav-menu{
+      position: absolute;
+      width: 264px;
+      height: 451px;
+      z-index: 9;
+      padding: 26px 0;
+      box-sizing: border-box;
+      background-color: #55585a7a;
+    }
+    .menu-wrap{
+      .menu-item{
+        height: 50px;
+        line-height: 50px;
+        &:hover{
+          background-color: $colorA;
+        }
+        a{
+          font-size: 16px;
+          color: #ffffff;
+          padding-left: 30px;
+          display: block;
+          position: relative;
+          &:after{
+            position: absolute;
+            right: 30px;
+            top: 17.5px;
+            content: "";
+            @include bgImg(10px,15px,'/imgs/icon-arrow.png')
+          }
+        }
+      }
+    }
+    .swiper-container {
+      height: 451px;
+      .swiper-button-prev{
+        left: 274px;
+      }
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+}
 </style>
