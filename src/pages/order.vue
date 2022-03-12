0e97bf8b57a1473893ecc840c7bd1865e0d465ed
尹志祥 <13602547976@163.com>
2022-03-12 11:00:54 +0800

    fix:carts

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-      <order-header></order-header>
+      <order-header :title="title">
+        <template v-slot:tip>
+          <span>{{tip}}</span>
+        </template>
+      </order-header>
       <router-view></router-view>
+      <service-bar></service-bar>
       <nav-footer></nav-footer>
   </div>
 </template>
@@ -9,11 +14,32 @@
 <script>
 import OrderHeader from '../components/OrderHeader.vue'
 import NavFooter from '../components/NavFooter.vue'
+import ServiceBar from '../components/ServiceBar.vue'
 export default {
   name: 'order',
   components: {
     OrderHeader,
-    NavFooter
+    NavFooter,
+    ServiceBar
+  },
+  data () {
+    return {
+      title: '',
+      tip: ''
+    }
+  },
+  mounted () {
+    let path = this.$route.path;
+    if(path == '/order/confirm'){
+      this.title = '订单确认';
+      this.tip ='请认真填写收货地址';
+    }else if(path == '/order/list'){
+      this.title = '订单列表';
+      this.tip = '请谨防钓鱼链接或诈骗电话，了解更多>';
+    }else if(path == '/order/pay'){
+      this.title = '订单支付';
+      this.tip = '请谨防钓鱼链接或诈骗电话，了解更多>';
+    }
   }
 }
 </script>
